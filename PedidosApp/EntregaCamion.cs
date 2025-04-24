859c6a400543c2665bc6f0175f6ceb5486a3cb8c
SebasG2806 <sebas.sebas1000399@gmail.com>
2025-04-23 21:33:15 -0500

    CLase entrega camion que hace la simulacion del costo

---
+++ PedidosApp/EntregaCamion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp
+{
+    public class EntregaCamion
+    {
+        public double CalcularCosto(int km) => 5.0 * km;
+        public string TipoEntrega() => "Camion";
+    }
+}
