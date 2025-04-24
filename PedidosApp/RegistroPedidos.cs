c425dfbe16738656e940ea5830d9db10970a7cc1
SebasG2806 <sebas.sebas1000399@gmail.com>
2025-04-23 22:14:33 -0500

    Clase singleton que inicializa la lista donde se almacenaran los pedidos

---
+++ PedidosApp/RegistroPedidos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp
+{
+    public sealed class RegistroPedidos
+    {
+        private static RegistroPedidos _Instancia = null;
+        private static readonly object _lock = new object();
+        public List<Pedido> Pedidos { get; private set; }
+        private RegistroPedidos()
+        {
+            Pedidos = new List<Pedido>();
+        }
+        public static RegistroPedidos Instancia
+        {
+            get
+            {
+                if(_Instancia == null)
+                {
+                    lock (_lock)
+                    {
+                        if(_Instancia == null)
+                        {
+                            _Instancia = new RegistroPedidos();
+                        }
+                    }
+                }
+                return _Instancia;
+            }
+        }
+        public void AgregarPedido(Pedido pedido)
+        {
+            if(pedido != null)
+            {
+                Pedidos.Add(pedido);
+            }
+        }
+    }
+}
